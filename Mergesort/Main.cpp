84f6c5b3e2e86606c20ebe9def6c3b18089fb6ca
In3xx <inxjang@gmail.com>
2022-08-04 00:57:01 +0900

    배열끼리 잘 나뉘나 값이 제대로 안나뉘어짐

--- Mergesort/Main.cpp
+++ Mergesort/Main.cpp
@@ -6,17 +6,17 @@
 
 using namespace std;
 
-#define ENDL cout << endl
+#define ENDL		cout << endl
+#define FREE(ptr)	{if(ptr){free(ptr); ptr = nullptr;}}
 
 void PrintArray(int*, int);
 void Divide(int*, int);
 int* Merge(int*, int, int*, int);
-void Free(int* arr);
 
 struct Array
 {
-	int size;
-	int arr[];
+	int size = 0;
+	int* arr = nullptr;
 };
 
 int main() {
@@ -28,17 +28,16 @@ int main() {
 	cout << "Max value : ";
 	cin >> maxValue;
 
-	srand((unsigned int)time(NULL));
+	srand((unsigned int)time(nullptr));
 	int* arr = (int*)malloc(sizeof(int) * arrSize);
 
 	for (int i = 0; i < arrSize; i++)
 		*(arr + i) = (rand() % maxValue + 1);
 
-	PrintArray(arr, arrSize);
-	ENDL;
+	//PrintArray(arr, arrSize);
+	//ENDL;
 
 	Divide(arr, arrSize);
-	Free(arr);
 
 	return 0;
 }
@@ -50,43 +49,114 @@ void PrintArray(int* arr, int arrSize) {
 }
 
 void Divide(int* arr, int arrSize) {
-	//int retCount = 0;
-	//int divideCnt = 2;
-	//int dividedSize = arrSize;
-	//while (dividedSize != 1)
-	//{
-	//	if (dividedSize % 2 != 0) {
-	//		dividedSize /= 2;
-	//		for (int i = 0, j = 0; i < divideCnt; i++, j = 0) {
-	//			cout << "[ ";
-	//			while (j < dividedSize)
-	//			{
-	//				cout << arr[j + (i * dividedSize)] << " ";
-	//				j++;
-	//			}
-	//			cout << "]    ";
-	//		}
-	//		ENDL;
-	//		divideCnt *= 2;
-	//	}
-	//	else {
-	//		dividedSize /= 2;
-	//		//if(dividedSize)
-	//	}
-	//}
-	//Free(arr);
-	/*int maxSize = arrSize;
-	int layer = 0;
-	int start, mid, last;
-	Array* dividedArr;
+	int maxSize = arrSize;
+	int printLayer = 1;
+	int count = 0;
+
+	//구조체 선언
+	Array* dividedArr = (Array*)malloc(sizeof(struct Array) * arrSize);
+	dividedArr[0].arr = (int*)malloc(sizeof(int) * arrSize);
+	
+	for (int i = 0; i < arrSize; i++)
+	{
+		dividedArr[0].size = arrSize;
+		dividedArr[0].arr[i] = arr[i];
+	}
+	FREE(arr);
+
+	PrintArray(dividedArr[0].arr, dividedArr[0].size);
+	ENDL;
 
 	do
 	{
-		for each (object var in collection_to_loop)
+		for (int i = 0; i < arrSize; i++)
 		{
+			//제일 큰배열부터 이분할
+			if (dividedArr[i].size >= maxSize) {
+				//left, right 사이즈 설정
+				int leftSize = dividedArr[i].size / 2;
+				int rightSize = dividedArr[i].size / 2 + dividedArr[i].size % 2;
 
+				//left, right 임시 할당
+				int* left = (int*)malloc(sizeof(int) * leftSize);
+				int* right = (int*)malloc(sizeof(int) * rightSize);
+
+				//left, right 원소값 넣기
+				for (int k = 0; k < dividedArr[i].size; k++)
+				{
+					if (k < leftSize)
+						left[k] = dividedArr[i].arr[k];
+					else
+						right[k - leftSize] = dividedArr[i].arr[k];
+				}
+				//임시출력
+				/*for (int l = 0; l < dividedArr[i].size; l++)
+				{
+					printf("LEFT(%d)  ", left[l]);
+					printf("RIGHT(%d)\n", right[l]);
+				}*/
+
+				//부모배열 할당해제
+				FREE(dividedArr[i].arr);
+				dividedArr[i].size = 0;
+
+				//left, right를 두 자리씩 넣기
+				for (int j = i; j < arrSize - 1; j++)
+				{
+					//left
+					dividedArr[j].arr = (int*)malloc(sizeof(int) * leftSize);
+					dividedArr[j].size = leftSize;
+					for (int x = 0; x < dividedArr[j].size; x++) {
+						dividedArr[j].arr[x] = left[x];
+					}
+
+					//right
+					if (dividedArr[j + 1].arr == nullptr) {
+						dividedArr[j + 1].arr = (int*)malloc(sizeof(int) * rightSize);
+						dividedArr[j + 1].size = rightSize;
+						for (int x = 0; x < dividedArr[j + 1].size; x++) {
+							dividedArr[j + 1].arr[x] = left[x];
+						}
+					}
+					else {
+						for (int u = arrSize - 1; u >= j + 1; u--)
+						{
+							dividedArr[u + 1] = dividedArr[u];
+						}
+						dividedArr[j + 1].arr = (int*)malloc(sizeof(int) * rightSize);
+						dividedArr[j + 1].size = rightSize;
+						for (int x = 0; x < dividedArr[j + 1].size; x++) {
+							dividedArr[j + 1].arr[x] = left[x];
+						}
+					}
+				}
+				FREE(left);
+				FREE(right);
+				count++;
+				//출력
+				if (count == printLayer || count == arrSize) {
+					count = 0;
+					printLayer *= 2;
+					for (int i = 0; i < printLayer; i++)
+					{
+						if (i < arrSize) {
+							printf("[ ");
+							for (int j = 0; j < dividedArr[i].size; j++)
+								printf("%d ", dividedArr[i].arr[j]);
+							printf("] ");
+						}
+						else break;
+					}
+					ENDL;
+				}
+				//maxSize 설정
+				maxSize = 0;
+				for (int i = 0; i < arrSize; i++)
+					if (maxSize < dividedArr[i].size)
+						maxSize = dividedArr[i].size;
+			}
 		}
-	} while (maxSize > 1);*/
+	} while (maxSize > 1);
 }
 
 int* Merge(int* left, int leftSize, int* right, int rightSize) {
@@ -119,16 +189,8 @@ int* Merge(int* left, int leftSize, int* right, int rightSize) {
 	PrintArray(ret, leftSize + rightSize);
 	ENDL;
 
-	Free(left);
-	Free(right);
+	FREE(left);
+	FREE(right);
 
 	return ret;
-}
-
-void Free(int* arr) {
-	if (arr != NULL) {
-		free(arr);
-		arr = NULL;
-	}
-	else return;
 }
